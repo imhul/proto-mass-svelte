6c4a494dff69d5e8da80af50b7d755eee5cbcb21
Zakhar Tkachuk <z.tkachuk@nodeart.io>
2022-06-14 18:37:41 +0300

    menu

--- src/lib/ui/header.svelte
+++ src/lib/ui/header.svelte
@@ -23,15 +23,35 @@
 </script>
 
 <header>
-	<a href="/" class="logo-wrapper">
-		<img src="" alt="logo" />
+	<a href="/" class="logo">
+		<i class="icon-i1"></i>
 	</a>
 
 	<nav>
 		<a href="/game" class="menu-link" on:click|preventDefault="{goGame}">
+			<!-- <i class="icon-Y15"></i> -->
 			<span>game</span>
 		</a>
+		<a
+			href="https://github.com/imhul/proto-mass-svelte/blob/master/README.mdx"
+			rel="noopener noreferrer"
+			target="_blank"
+			class="menu-link"
+		>
+			<!-- <i class="icon-Y15"></i> -->
+			<span>About</span>
+		</a>
+		<a
+			href="https://www.patreon.com/protomass"
+			rel="noopener noreferrer"
+			target="_blank"
+			class="menu-link"
+		>
+			<!-- <i class="icon-dollar3"></i> -->
+			<span>Donate</span>
+		</a>
 		<span class="menu-link" on:click="{isLoggedIn ? logout : login}">
+			<!-- <i class="icon-Y15"></i> -->
 			<span>{isLoggedIn ? 'logout' : 'login'}</span>
 		</span>
 	</nav>
@@ -39,7 +59,55 @@
 
 <style lang="scss">
 	header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
 		height: 5rem;
 		width: 100%;
+		padding: 0 var(--size-30);
+		background-color: var(--game-color-dark);
+
+		.logo {
+			font-size: var(--size-32);
+			color: var(--game-color);
+			text-shadow: 2px 2px 0px var(--game-color-warn);
+
+			&:hover {
+				color: var(--game-color-darkest);
+				// text-shadow: 2px 2px 0px var(--game-color-warn);
+			}
+		}
+
+		nav {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+
+			.menu-link {
+				display: block;
+				height: 5rem;
+				padding: 0 var(--size-20);
+				line-height: 5rem;
+				text-transform: uppercase;
+				background: var(--game-color-dark);
+				text-shadow: 2px 2px 0px var(--game-color-warn);
+
+				span {
+					font-size: var(--size-22);
+					font-family: var(--8-bit);
+					color: var(--game-color);
+				}
+
+				&:hover {
+					background-color: var(--game-color);
+					span,
+					i,
+					a {
+						color: var(--game-color-darkest);
+						text-shadow: 2px 2px 0px var(--game-color-warn);
+					}
+				}
+			}
+		}
 	}
 </style>
--- src/lib/ui/hero.svelte
+++ src/lib/ui/hero.svelte
@@ -1,5 +1,4 @@
-<h1>Proto-Mass<br /><i class="icon-i1" /></h1>
-
+<h1>Proto-Mass<br /><i class="icon-i1"></i></h1>
 
 <style lang="scss">
 	h1 {
@@ -16,5 +15,11 @@
 		text-shadow: 4px 4px 0 #ff0045, 8px 8px 0 #5500ff;
 		margin: 0 0 10px;
 		animation: playHero 20s ease-in-out;
+		user-select: none;
+		touch-action: none;
+
+		&::selection {
+			color: #ff85e5;
+		}
 	}
 </style>
